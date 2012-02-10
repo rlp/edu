fa0353d0b99e4958049387e245dc68fe7526d889
Niles Rowland <niles@reallocalpages.com>
2012-02-10 16:33:58 -0500

    create migration to rename details table to user_details. CakeDc screw-up.

---
+++ Config/Migration/001_rename_details_table_to_user_details_to_fix_bug_in_users_plugin.php
@@ -0,0 +1,52 @@
+<?php
+class M4f3589cb9c604bad9e3b3e8092b40148 extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'rename_table' => array(
+				'details' => 'user_details',
+			),
+		),
+		'down' => array(
+			'rename_table' => array(
+				'user_details' => 'details',
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
